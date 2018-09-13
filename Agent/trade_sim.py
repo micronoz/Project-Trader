576ebf44c60fa4ff1c58feca84b664281e3037c2
Nabi Ozberkman <nabiozberkman@Nabis-Mac-Pro.local>
2018-09-13 23:04:47 +0300

    Model testing under construction.

--- Agent/trade_sim.py
+++ Agent/trade_sim.py
@@ -177,7 +177,7 @@ class Market:
         dimensions = ['Open', 'High', 'Low']
         count = 0
         for timeIndex in range(startIndex, (startIndex + size) if (startIndex + size) < len(lastDate) else len(lastDate)-1):
-          
+            print(count)
             count += 1
             
             m = 0
